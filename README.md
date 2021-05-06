abd2033eed15771e52c1f6a9539b96b42efe6e05
juliocesarscheidt <julio.scheidt96@gmail.com>
2021-05-06 02:49:24 -0300

    Minor changes and improvements
    Signed-off-by: juliocesarscheidt <julio.scheidt96@gmail.com>

--- README.md
+++ README.md
@@ -11,12 +11,13 @@
 > In order to run the deployments in K8S, run the script deploy.sh
 
 ```bash
-chmod +x ./deploy.sh && bash ./deploy.sh [canary|mirror]
+chmod +x ./deploy.sh && \
+    bash ./deploy.sh [canary|mirror]
 
 # to see info about K8S deployed objects
-kubectl get pod,deploy,svc,hpa -n istio-project -o wide
+kubectl get pod,deploy,svc,hpa -n istioproject -o wide
 
-kubectl get virtualservice,gateway,destinationrule -n istio-project -o wide
+kubectl get virtualservice,gateway,destinationrule -n istioproject -o wide
 
 
 kubectl get pod,deploy,svc -n istio-system -o wide
--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,26 @@ set -- "$1"
 
 # canary, mirror
 DEPLOY_TYPE=${1:-"canary"}
-VIRTUAL_SERVICE_FILE="./api-virtual-svc-${DEPLOY_TYPE,,}.yaml"
+VIRTUAL_SERVICE_FILE="./virtualservice${DEPLOY_TYPE,,}.yaml"
 
-kubectl apply -f ./api.yaml
-
-kubectl apply -f ./api-gateway.yaml
+kubectl apply -f ./namespace.yaml
+kubectl apply -f ./deployment.yaml
+kubectl apply -f ./service.yaml
+kubectl apply -f ./apigateway.yaml
 kubectl apply -f "${VIRTUAL_SERVICE_FILE}"
-kubectl apply -f ./api-destination-rule.yaml
+kubectl apply -f ./destinationrule.yaml
 
-CURRENT_CONTEXT=$(kubectl config view | grep "current-context" | cut -d ":" -f2 | tr -d ' ')
-kubectl config set-context "${CURRENT_CONTEXT}" --namespace=istio-project
+# CURRENT_CONTEXT=$(kubectl config view | grep "current-context" | cut -d ":" -f2 | tr -d ' ')
+# kubectl config set-context "${CURRENT_CONTEXT}" --namespace=istioproject
+
+# kubectl get -n istioproject gateway.networking.istio.io/api-gateway
+# kubectl describe -n istioproject gateway.networking.istio.io/api-gateway
+
+# kubectl get -n istioproject virtualservice.networking.istio.io/api
+# kubectl describe -n istioproject virtualservice.networking.istio.io/api
+
+# kubectl get -n istioproject destinationrule.networking.istio.io/destinationrule
+# kubectl describe -n istioproject destinationrule.networking.istio.io/destinationrule
+
+# kubectl autoscale -n istioproject deployment api-deployment-v1 --cpu-percent=50 --min=1 --max=10
+# kubectl logs -n istioproject -f ${POD_NAME} -c istio-proxy
